d51c47f4b879c3d4cb1d9ba9052f9c68c9a6bc64
ALLAN ALVES DE AQUINO <16254826@CFP127ALUNO.LOCAL>
2018-10-25 16:43:32 -0200

    Criando base para view de login

--- src/br/com/food4fit/Main.java
+++ src/br/com/food4fit/Main.java
@@ -1,26 +1,71 @@
 package br.com.food4fit;
-	
+
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.geometry.Rectangle2D;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 
 
 public class Main extends Application {
+
+	static Stage primaryStage;
+
 	@Override
 	public void start(Stage primaryStage) {
+		Main.primaryStage = primaryStage;
+
+		Main.primaryStage.setResizable(false);
+		Main.primaryStage.setMaximized(true);
+
+		Screen screen = Screen.getPrimary();
+		Rectangle2D bounds = screen.getVisualBounds();
+
+		primaryStage.setX(bounds.getMinX());
+		primaryStage.setY(bounds.getMinY());
+		primaryStage.setMaxWidth(bounds.getWidth());
+		primaryStage.setMinWidth(bounds.getWidth());
+		primaryStage.setMaxHeight(bounds.getHeight());
+		primaryStage.setMinHeight(bounds.getHeight());
+
+
+		Main.abrirTela("Login");
+
+	}
+
+
+	public static void abrirTela(String nomeArquivo){
+		Parent tela;
+
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
+			//Carregando xml no objeto
+			tela = FXMLLoader.load(Main.class.getResource("view/"+ nomeArquivo + ".fxml"));
+
+			//Criando o objeto de "cena" com xml
+			Scene sc = new Scene(tela);
+
+			//Colocando o objeto fde cena no Stage
+			primaryStage.setScene(sc);
+
+			//Exibindo na tela
 			primaryStage.show();
-		} catch(Exception e) {
+
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
+
+		//scene.getStylesheets().add(getClass().getResource("css/application.css").toExternalForm());
+
 	}
-	
+
+
 	public static void main(String[] args) {
 		launch(args);
 	}
+
+
+
 }
